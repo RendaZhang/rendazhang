d168800c8668ba9260c0015b3c22de3bcfa257a4
张人大 <952402967@qq.com>
2025-07-28 03:34:58 +0800

    Refactor about page for dynamic language

--- public/js/about.js
+++ public/js/about.js
@@ -1,6 +1,7 @@
 (function () {
   function applyLang() {
     var data = window.aboutPageData || {};
+    var contentData = window.aboutPageContent || {};
     var storedLang = localStorage.getItem('preferred_lang');
     var lang = storedLang || 'zh-CN';
     var showZh = lang.startsWith('zh');
@@ -12,6 +13,46 @@
       document.title = showZh ? data.titles.zh : data.titles.en;
       if (meta) {
         meta.setAttribute('content', showZh ? data.descriptions.zh : data.descriptions.en);
+      }
+    }
+
+    if (contentData.zh && contentData.en) {
+      var current = showZh ? contentData.zh : contentData.en;
+      var container = document.getElementById('content');
+      if (container) {
+        var h1 = container.querySelector('#heroHeading');
+        if (h1) h1.innerHTML = current.heroHeading;
+        var title = container.querySelector('#aboutTitle');
+        if (title) title.textContent = current.title;
+        var ps = container.querySelector('#aboutParagraphs');
+        if (ps) {
+          ps.innerHTML = '';
+          current.paragraphs.forEach(function (p) {
+            var el = document.createElement('p');
+            el.textContent = p;
+            ps.appendChild(el);
+          });
+        }
+        var info = container.querySelector('#aboutInfo');
+        if (info) {
+          info.innerHTML = '';
+          current.info.forEach(function (item) {
+            var li = document.createElement('li');
+            var span = document.createElement('span');
+            span.textContent = item.label;
+            li.appendChild(span);
+            li.appendChild(document.createTextNode(item.value));
+            info.appendChild(li);
+          });
+        }
+        var resume = container.querySelector('#resumeLink');
+        if (resume) {
+          resume.textContent = current.resumeLabel;
+          if (data.resumePaths && data.resumeDownloads) {
+            resume.setAttribute('href', showZh ? data.resumePaths.zh : data.resumePaths.en);
+            resume.setAttribute('download', showZh ? data.resumeDownloads.zh : data.resumeDownloads.en);
+          }
+        }
       }
     }
   }
