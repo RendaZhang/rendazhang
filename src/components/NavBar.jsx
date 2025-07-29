392364095ac79afac84899f59cb3939d0df10368
Renda Zhang <rendazhang@qq.com>
2025-07-29 16:43:39 +0800

    Remove login and register from nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import ThemeToggle from './ThemeToggle.jsx';
 import LanguageSwitcher from './LanguageSwitcher.jsx';
 import HamburgerMenu from './HamburgerMenu.jsx';
-import { HOME_PAGE_PATH, LOGIN_PAGE_PATH, REGISTER_PAGE_PATH } from '../config.js';
+import { HOME_PAGE_PATH, LOGIN_PAGE_PATH } from '../config.js';
+import AvatarIcon from './AvatarIcon.jsx';
 import { NAV_CONTENT } from '../content/navContent.js';
-import { useLanguage } from '../context/LanguageContext.jsx';
 import LocalizedSection from './LocalizedSection.jsx';
 
 export default function NavBar() {
-  const languageContext = useLanguage() || {};
-
   // 不再根据当前语言只渲染一种文本，避免刷新时语言切换产生闪烁
   const textsEn = NAV_CONTENT.en;
   const textsZh = NAV_CONTENT.zh;
@@ -20,14 +18,11 @@ export default function NavBar() {
       <a href={HOME_PAGE_PATH}>
         <LocalizedSection zhContent={textsZh.home} enContent={textsEn.home} />
       </a>
-      <a href={LOGIN_PAGE_PATH}>
-        <LocalizedSection zhContent={textsZh.login} enContent={textsEn.login} />
-      </a>
-      <a href={REGISTER_PAGE_PATH}>
-        <LocalizedSection zhContent={textsZh.register} enContent={textsEn.register} />
-      </a>
       <LanguageSwitcher />
       <ThemeToggle />
+      <a href={LOGIN_PAGE_PATH} aria-label="Login" className="avatar-link">
+        <AvatarIcon />
+      </a>
     </nav>
   );
 }
