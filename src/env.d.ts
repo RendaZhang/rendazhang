da7745c011249358f4f738dc37587a501044ec3d
Renda Zhang <rendazhang@qq.com>
2025-08-07 14:21:42 +0800

    Add ImportMetaEnv interface in env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -22,3 +22,37 @@ declare module '*.astro' {
   const component: AstroComponent;
   export default component;
 }
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      readonly PUBLIC_API_BASE_URL?: string;
+      readonly PUBLIC_CDN_BASE?: string;
+      readonly PUBLIC_NODE_ENV?: string;
+      readonly PUBLIC_SENTRY_DSN?: string;
+      readonly PUBLIC_SITE_BASE_URL?: string;
+      readonly PUBLIC_TAG_NAME?: string;
+      readonly SENTRY_DSN?: string;
+      readonly SENTRY_AUTH_TOKEN?: string;
+      readonly SENTRY_ORG?: string;
+      readonly SENTRY_PROJECT?: string;
+      readonly SKIP_SENTRY?: string;
+      readonly NODE_ENV?: string;
+    }
+  }
+
+  interface ImportMetaEnv {
+    readonly PUBLIC_API_BASE_URL: string;
+    readonly PUBLIC_CDN_BASE: string;
+    readonly PUBLIC_NODE_ENV: string;
+    readonly PUBLIC_SENTRY_DSN: string;
+    readonly PUBLIC_SITE_BASE_URL: string;
+    readonly PUBLIC_TAG_NAME: string;
+  }
+
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+}
+
+export {};
