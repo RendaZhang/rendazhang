6619faa8bf0594d57a92e03e0ad67a423b6302d8
RendaZhang <RendaZhang@users.noreply.github.com>
2025-08-08 23:04:04 +0000

    Release v1.0.1 14ba032f3980a4b35df923fc61ae12d92d1139d5

---
+++ _astro/chunk-E2GYISFI.LI9lKO7n.js
@@ -0,0 +1,16 @@
+import{_ as o}from"./useMarkdownPipeline.BKkcsd5_.js";(function(){var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"14ba032f3980a4b35df923fc61ae12d92d1139d5"}})();try{(function(){var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},n=new e.Error().stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="57c7f9b9-1af7-407c-ad2f-9e88ff224451",e._sentryDebugIdIdentifier="sentry-dbid-57c7f9b9-1af7-407c-ad2f-9e88ff224451")})()}catch{}var f=o(()=>`
+  /* Font Awesome icon styling - consolidated */
+  .label-icon {
+    display: inline-block;
+    height: 1em;
+    overflow: visible;
+    vertical-align: -0.125em;
+  }
+  
+  .node .label-icon path {
+    fill: currentColor;
+    stroke: revert;
+    stroke-width: revert;
+  }
+`,"getIconStyles");export{f as g};
+//# sourceMappingURL=chunk-E2GYISFI.LI9lKO7n.js.map
