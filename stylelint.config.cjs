280130640921f3b2617bcace90451068850fc1c7
Renda Zhang <rendazhang@qq.com>
2025-08-10 03:57:44 +0800

    Extend Stylelint configuration and setup

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,6 +1,13 @@
 module.exports = {
+  plugins: ['stylelint-order', 'stylelint-declaration-block-no-ignored-properties'],
   rules: {
     'declaration-no-important': true,
-    'max-nesting-depth': 3
+    'max-nesting-depth': 3,
+    'selector-class-pattern': '^(c|u|is|js)-[a-z0-9-]+$',
+    'no-descending-specificity': true,
+    'custom-property-pattern': '^--[a-z0-9-]+$',
+    'plugin/declaration-block-no-ignored-properties': true,
+    'order/order': [['custom-properties', 'declarations']],
+    'order/properties-alphabetical-order': true
   }
 };
